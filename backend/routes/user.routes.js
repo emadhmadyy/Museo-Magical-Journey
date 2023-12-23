72b6588a3bcb02c413a0adee339797cc192a3e64
Emad Hmady <emadhmady0@gmail.com>
2023-12-24 00:29:25 +0200

    created user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const { login, register } = require("../controllers/user.controllers");
 
-router.post("/register", register);
 router.post("/login", login);
+router.post("/register", register);
 
 module.exports = router;
