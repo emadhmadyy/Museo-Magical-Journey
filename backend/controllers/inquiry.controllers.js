bef280585c9a710e652c4111c62be6ac327e5152
Emad Hmady <emadhmady0@gmail.com>
2024-01-05 04:22:48 +0200

    backend: edited inquiry controller

--- backend/controllers/inquiry.controllers.js
+++ backend/controllers/inquiry.controllers.js
@@ -3,7 +3,7 @@ const Inquiry = require("../models/inquiry.model");
 const addInquiry = async (req, res) => {
   const { email, first_name, last_name, phone_number, inquiry_text } = req.body;
   if (!email || !first_name || !last_name || !phone_number || !inquiry_text) {
-    res.status(400).send({ message: "All fields are required" });
+    return res.status(400).send({ message: "All fields are required" });
   }
   try {
     const inquiry = new Inquiry({
@@ -14,9 +14,9 @@ const addInquiry = async (req, res) => {
       inquiry_text,
     });
     await inquiry.save();
-    res.status(200).send({ message: "Inquiry added successfully" });
+    return res.status(200).send({ message: "Inquiry added successfully" });
   } catch (e) {
-    res.status(500).send({ error: e });
+    return res.status(500).send({ error: e });
   }
 };
 
