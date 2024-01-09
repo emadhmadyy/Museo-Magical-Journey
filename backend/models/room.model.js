03806bad703eab05f102ad35ccab35ddaaa9ad76
Emad Hmady <emadhmady0@gmail.com>
2024-01-09 13:08:13 +0200

    backend <> edited room model

--- backend/models/room.model.js
+++ backend/models/room.model.js
@@ -1,21 +1,15 @@
 const mongoose = require("mongoose");
 
-const roomSchema = new mongoose.Schema(
-  {
-    users: [
-      {
-        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-        character: { type: mongoose.Schema.Types.ObjectId, ref: "Character" },
-        isHost: { type: Boolean },
-      },
-    ],
-    status: { type: String, default: "Open", enum: ["Open", "Closed"] },
-  },
-  {
-    autoCreate: false,
-    autoIndex: false,
-  }
-);
+const roomSchema = new mongoose.Schema({
+  users: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      character: { type: mongoose.Schema.Types.ObjectId, ref: "Character" },
+      isHost: { type: Boolean },
+    },
+  ],
+  status: { type: String, default: "Open", enum: ["Open", "Closed"] },
+});
 
 const validateUsersArrayLength = function (users) {
   return users.length <= 10;
@@ -26,4 +20,3 @@ roomSchema.path("users").validate(validateUsersArrayLength, "Room is full");
 const Room = mongoose.model("Room", roomSchema);
 
 module.exports = Room;
-//autocreate autoindex set them both to false.
