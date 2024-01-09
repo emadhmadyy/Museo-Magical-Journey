f17341c8b0b00a81fe3e8f6fe46e5d36329fc50e
Emad Hmady <emadhmady0@gmail.com>
2024-01-10 01:56:00 +0200

    frontend <> created routes for all the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,23 @@ import "./App.css";
 // import InfoCard from "./components/infoCard";
 // import icon from "./assets/images/icons8-statue.png";
 import Landing from "./pages/landing";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Options from "./pages/options";
+import Tour from "./pages/tour";
+import TimeTravelMenu from "./pages/timeTravelMenu";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/options" element={<Options />} />
+          <Route path="/tour" element={<Tour />} />
+          <Route path="/time-travel" element={<TimeTravelMenu />} />
         </Routes>
       </BrowserRouter>
       {/* <Nav></Nav>
---
+++ frontend/src/pages/timeTravelMenu/index.jsx
@@ -0,0 +1,6 @@
+import "./index.css";
+const TimeTravelMenu = () => {
+  return;
+};
+
+export default TimeTravelMenu;
