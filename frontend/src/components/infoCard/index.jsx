2940cde6b30de9da33aad866fbf349fcc5467e2f
Emad Hmady <emadhmady0@gmail.com>
2024-01-09 22:14:21 +0200

    frontend <> added infoCard icons

--- frontend/src/components/infoCard/index.jsx
+++ frontend/src/components/infoCard/index.jsx
@@ -1,1 +1,16 @@
 import "./index.css";
+
+const InfoCard = () => {
+  return (
+    <>
+      <img src="" alt="icon" />
+      <p>title</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed
+        molestie enim, vitae vehicula est. Nam ac nisl nisl.
+      </p>
+    </>
+  );
+};
+
+export default InfoCard;
