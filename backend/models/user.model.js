e4f9290c94a9967279e43987fd0986ce83ce4e76
Emad Hmady <emadhmady0@gmail.com>
2024-01-09 13:10:33 +0200

    backend <> edited user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,41 +1,36 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-    minlength: 3,
-    maxlength: 26,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-  },
+const userschema = new mongoose.Schema({
   first_name: {
     type: String,
     required: true,
-    minlength: 2,
   },
   last_name: {
     type: String,
     required: true,
-    minlength: 2,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
   },
   user_type: {
     type: Number,
     required: true,
     maxlength: 1,
   },
-  profile_url: {
-    type: String,
-  },
 });
 
-userSchema.pre(
+userschema.pre(
   "save",
   async function (next) {
     try {
@@ -52,6 +47,5 @@ userSchema.pre(
   }
 );
 
-const User = mongoose.model("User", userSchema);
-
+const User = mongoose.model("User", userschema);
 module.exports = User;
