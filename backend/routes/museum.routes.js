0275bab7592a2b2b2d47c7b8b61619976a0a978d
Emad Hmady <emadhmady0@gmail.com>
2024-01-09 13:02:25 +0200

    backend <> deleted museum routes

--- backend/routes/museum.routes.js
+++
@@ -1,6 +0,0 @@
-var express = require("express");
-const router = express.Router();
-const { createMuseum } = require("../controllers/museum.controllers");
-router.post("/", createMuseum);
-
-module.exports = router;
