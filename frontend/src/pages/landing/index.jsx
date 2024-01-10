66aed28a15754e2c4e68f63736042913173e6bd0
Emad Hmady <emadhmady0@gmail.com>
2024-01-10 03:01:13 +0200

    frontend <> created about section css

--- frontend/src/pages/landing/index.jsx
+++ frontend/src/pages/landing/index.jsx
@@ -4,14 +4,20 @@ const Landing = () => {
   return (
     <>
       <Nav></Nav>
-      <div className="flex column main-padding">
-        <p>
-          Welcome to our immersive 3D museum, where art comes to life. Explore a
-          captivating collection of statues that blend history, culture, and
-          creativity. Each masterpiece tells a unique story, inviting you to
-          embark on a visual journey through time. Join us in celebrating the
-          beauty and significance of sculpted art in our digital realm.
-        </p>
+      <div className="flex column main-padding about-container">
+        <div className="blur-container">
+          <div className="blur-text">
+            <p>
+              Welcome to our immersive 3D museum, where art comes to life.
+              Explore a captivating collection of statues that blend history,
+              culture, and creativity. Each masterpiece tells a unique story,
+              inviting you to embark on a visual journey through time. Join us
+              in celebrating the beauty and significance of sculpted art in our
+              digital realm.
+            </p>
+          </div>
+          <div className="blur-background"></div>
+        </div>
         <button className="get-started-btn secondary-color white-font">
           Get Started
         </button>
