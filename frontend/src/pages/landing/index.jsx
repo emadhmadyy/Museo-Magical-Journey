64381a127943d33f773d157217ad3476e382d540
Emad Hmady <emadhmady0@gmail.com>
2024-01-10 11:44:06 +0200

    frontend <> created features component

--- frontend/src/pages/landing/index.jsx
+++ frontend/src/pages/landing/index.jsx
@@ -2,69 +2,15 @@ import "./index.css";
 import Nav from "../../components/nav";
 import Footer from "../../components/footer";
 import About from "../../components/about";
-import InfoCard from "../../components/infoCard";
+import Features from "../../components/features";
 import Input from "../../components/inputField";
-import statue from "../../assets/images/icons8-statue.png";
-import team from "../../assets/images/icons8-team.png";
-import chat from "../../assets/images/icons8-chat.png";
-import time from "../../assets/images/icons8-time-machine.png";
+
 const Landing = () => {
-  const info = [
-    {
-      icon: statue,
-      title: "3D Masterpieces",
-      description:
-        "Immerse yourself in an extraordinary collection of 3D artworks that transcend time and space. Explore meticulously crafted sculptures and paintings, each brought to life in stunning detail within our digital museum",
-    },
-    {
-      icon: team,
-      title: "Group Virtual Tours",
-      description:
-        "Embark on shared adventures with friends, family, or fellow art enthusiasts through our group virtual tours. Navigate the museum's diverse exhibits together, fostering a sense of community and shared exploration in the world of art.",
-    },
-    {
-      icon: chat,
-      title: " Real-time Chat",
-      description:
-        "Connect with your group through our real-time chat feature. Share thoughts, insights, and reactions as you explore the virtual galleries together. Experience the joy of art in a dynamic and interactive community.",
-    },
-    {
-      icon: time,
-      title: "Time Travel",
-      description:
-        "Unleash the magic of time travel with our unique time machine. Witness the evolution of art across different eras as you scroll through captivating scenes. Experience the seamless blend of history and innovation in an enchanting visual journey.",
-    },
-  ];
   return (
     <>
       <Nav />
       <About />
-      <div className="features flex column">
-        <p className="features-section-title">More Than A Regular Museum</p>
-        <p className="features-section-subtitle">Explore Our Super Features</p>
-        <div className="flex cards-container">
-          <InfoCard
-            icon={info[0].icon}
-            title={info[0].title}
-            description={info[0].description}
-          />
-          <InfoCard
-            icon={info[1].icon}
-            title={info[1].title}
-            description={info[1].description}
-          />
-          <InfoCard
-            icon={info[2].icon}
-            title={info[2].title}
-            description={info[2].description}
-          />
-          <InfoCard
-            icon={info[3].icon}
-            title={info[3].title}
-            description={info[3].description}
-          />
-        </div>
-      </div>
+      <Features />
       <div className="main-padding main-color inquiry-form-container flex column">
         <p className="contact-us-title">Contact Us</p>
         <div className="inquiry-form box-shadow flex column">
