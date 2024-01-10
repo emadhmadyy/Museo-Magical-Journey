d4d92627213a7d4b10620f0e6996d25396caa83b
Emad Hmady <emadhmady0@gmail.com>
2024-01-11 01:53:07 +0200

    frontend <> created signup form

--- frontend/src/pages/register/index.jsx
+++ frontend/src/pages/register/index.jsx
@@ -1,6 +1,106 @@
 import "./index.css";
+import Nav from "../../components/nav";
+import Footer from "../../components/footer";
+import Input from "../../components/inputField";
 const Register = () => {
-  return;
+  return (
+    <>
+      <Nav />
+      <div className="signup-container main-color main-padding flex">
+        <div className="signup-form box-shadow flex column">
+          <p className="signup-title">sign up</p>
+          <Input
+            type="text"
+            placeholder="First Name"
+            value=""
+            onChange=""
+            error=""
+            name="first-name"
+            labelName="first name"
+          />
+          <Input
+            type="text"
+            placeholder="Last Name"
+            value=""
+            onChange=""
+            error=""
+            name="last-name"
+            labelName="last name"
+          />
+          <Input
+            type="email"
+            placeholder="Email"
+            value=""
+            onChange=""
+            error=""
+            name="email"
+            labelName="email"
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            value=""
+            onChange=""
+            error=""
+            name="password"
+            labelName="password"
+          />
+          <button className="signup-btn-register-form secondary-color white-font">
+            SIGN UP
+          </button>
+          <p>
+            have an account? <span className="signin-btn">log in</span>
+          </p>
+        </div>
+        <div className="signup-form box-shadow flex column">
+          <p className="signup-title">sign up</p>
+          <Input
+            type="text"
+            placeholder="First Name"
+            value=""
+            onChange=""
+            error=""
+            name="first-name"
+            labelName="first name"
+          />
+          <Input
+            type="text"
+            placeholder="Last Name"
+            value=""
+            onChange=""
+            error=""
+            name="last-name"
+            labelName="last name"
+          />
+          <Input
+            type="email"
+            placeholder="Email"
+            value=""
+            onChange=""
+            error=""
+            name="email"
+            labelName="email"
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            value=""
+            onChange=""
+            error=""
+            name="password"
+            labelName="password"
+          />
+          <button className="signup-btn-register-form secondary-color white-font">
+            SIGN UP
+          </button>
+          <p>
+            have an account? <span className="signin-btn">log in</span>
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Register;
