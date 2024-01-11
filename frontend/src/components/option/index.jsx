53bd3a84aaebd79b87d202f8981087c2dfc900b7
Emad Hmady <emadhmady0@gmail.com>
2024-01-11 09:45:16 +0200

    frontend <> created option component css

--- frontend/src/components/option/index.jsx
+++ frontend/src/components/option/index.jsx
@@ -2,10 +2,12 @@
 import "./index.css";
 const Option = ({ title, paragraph1, paragraph2, btn_text }) => {
   return (
-    <div className="option-container">
+    <div className="option-container flex column  main-color">
       <p className="option-title">{title}</p>
-      <p className="option-paragraaph">{paragraph1}</p>
-      <p className="option-paragraph">{paragraph2}</p>
+      <div className="paragraphs">
+        <p className="option-paragraph">{paragraph1}</p>
+        <p className="option-paragraph">{paragraph2}</p>
+      </div>
       <button className="option-btn">{btn_text}</button>
     </div>
   );
--- frontend/src/pages/options/index.jsx
+++ frontend/src/pages/options/index.jsx
@@ -1,10 +1,27 @@
 import "./index.css";
 import Nav from "../../components/nav";
 import Footer from "../../components/footer";
+import Option from "../../components/option";
 const Options = () => {
+  const options = [
+    {
+      title: "Virtual Tour",
+      paragraph1:
+        "Whether you’re an art enthusiast or simply looking for a fun and educational way to spend your time, our virtual museum tour is the perfect choice.",
+      paragraph2:
+        "With our cutting-edge technology, you can explore the museum from the comfort of your own home and experience the thrill of being up close and personal with these incredible works of art.",
+      btn_text: "TAKE A VIRTUAL TOUR",
+    },
+  ];
   return (
     <>
       <Nav />
+      <Option
+        title={options[0].title}
+        paragraph1={options[0].paragraph1}
+        paragraph2={options[0].paragraph2}
+        btn_text={options[0].btn_text}
+      />
       <Footer />
     </>
   );
