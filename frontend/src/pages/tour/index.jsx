31c7ad25da89f1ab8102d5f59ecf98e8131d385f
Emad Hmady <emadhmady0@gmail.com>
2024-01-12 07:16:44 +0200

    frontend <> created tour page component

--- frontend/src/pages/tour/index.jsx
+++ frontend/src/pages/tour/index.jsx
@@ -1,4 +1,7 @@
 import "./index.css";
+import Nav from "../../components/nav";
+import Footer from "../../components/footer";
+import Option from "../../components/option";
 const Tour = () => {
   return;
 };
