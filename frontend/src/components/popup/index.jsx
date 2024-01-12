6b8c2e15595f5949ccc6f5b8c2633ebe4a0d861d
Emad Hmady <emadhmady0@gmail.com>
2024-01-12 09:41:20 +0200

    frontend <> added overlay css properties

--- frontend/src/components/popup/index.jsx
+++ frontend/src/components/popup/index.jsx
@@ -3,7 +3,7 @@ import Input from "../inputField";
 const Popup = () => {
   return (
     <div className="overlay">
-      <div className="popup flex column">
+      <div className="popup flex column main-color">
         <button className="close-btn"> x </button>
         <Input
           type="text"
@@ -17,7 +17,7 @@ const Popup = () => {
         <button className="popup-btn secondary-color white-font">
           Join Group Tour
         </button>
-        <button className="popup-btn secondary-color white-font">
+        <button className="popup-btn secondary-color white-font host-btn">
           Host Group Tour
         </button>
       </div>
