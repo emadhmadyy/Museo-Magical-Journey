2b1192cfd37fdaf4f8ba638c050d3e86d5f9f46a
Emad Hmady <emadhmady0@gmail.com>
2024-01-12 06:54:00 +0200

    frontend <> edited sign up button

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -9,39 +9,8 @@ const Login = () => {
     <>
       <Nav />
       <div className="flex main-padding login-container main-color">
-        {/* <div className="model"></div> */}
         <div className="login-form flex column box-shadow">
-          <p className="login-title">log in</p>
-          <Input
-            type="email"
-            placeholder="Email"
-            value=""
-            onChange=""
-            error=""
-            name="login-email"
-            labelName="email"
-          />
-          <Input
-            type="password"
-            placeholder="Password"
-            value=""
-            onChange=""
-            error=""
-            name="login-password"
-            labelName="password"
-          />
-          <button className="secondary-color white-font login-btn">
-            LOG IN
-          </button>
-          <p className="">
-            dont have an account?{" "}
-            <span className="signup-btn" onClick={() => navigate("/register")}>
-              sign up
-            </span>
-          </p>
-        </div>
-        <div className="login-form flex column box-shadow">
-          <p className="login-title">log in</p>
+          <p className="login-title">Log In</p>
           <Input
             type="email"
             placeholder="Email"
@@ -49,7 +18,7 @@ const Login = () => {
             onChange=""
             error=""
             name="email"
-            labelName="email"
+            labelName="Email"
           />
           <Input
             type="password"
@@ -58,13 +27,46 @@ const Login = () => {
             onChange=""
             error=""
             name="password"
-            labelName="password"
+            labelName="Password"
           />
           <button className="secondary-color white-font login-btn">
             LOG IN
           </button>
           <p className="">
-            dont have an account? <span className="signup-btn">sign up</span>
+            dont have an account?{" "}
+            <span className="signup-btn" onClick={() => navigate("/register")}>
+              Sign Up
+            </span>
+          </p>
+        </div>
+        <div className="login-form flex column box-shadow">
+          <p className="login-title">Log In</p>
+          <Input
+            type="email"
+            placeholder="Email"
+            value=""
+            onChange=""
+            error=""
+            name="email"
+            labelName="Email"
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            value=""
+            onChange=""
+            error=""
+            name="password"
+            labelName="Password"
+          />
+          <button className="secondary-color white-font login-btn">
+            LOG IN
+          </button>
+          <p className="">
+            dont have an account?{" "}
+            <span className="signup-btn" onClick={() => navigate("/register")}>
+              Sign Up
+            </span>
           </p>
         </div>
       </div>
