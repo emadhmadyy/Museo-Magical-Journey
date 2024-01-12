437c3a23e61a5133f4a8c460ebc0dfae559b374c
Emad Hmady <emadhmady0@gmail.com>
2024-01-12 08:25:56 +0200

    frontend <> created options array in tour index.jsx

--- frontend/src/pages/tour/index.jsx
+++ frontend/src/pages/tour/index.jsx
@@ -3,6 +3,24 @@ import Nav from "../../components/nav";
 import Footer from "../../components/footer";
 import Option from "../../components/option";
 const Tour = () => {
+  const options = [
+    {
+      title: "Solo Virtual Tour",
+      paragraph1:
+        "Whether you are an art enthusiast or simply looking for a fun and educational way to spend your time, a tour in our virtual museum is the perfect choice.",
+      paragraph2:
+        "With our cutting-edge technology, you can explore the museum from the comfort of your own home and experience the thrill of being so close to these incredible works of art.",
+      btn_text: "Go Solo",
+    },
+    {
+      title: "Group Virtual Tour",
+      paragraph1:
+        "Whether you are an art enthusiast or simply looking for a fun and educational way to spend your time, a tour in our virtual museum is the perfect choice.",
+      paragraph2:
+        "With our cutting-edge technology, you can explore the museum from the comfort of your own home and experience the thrill of being so close to these incredible works of art.",
+      btn_text: "Join / Host",
+    },
+  ];
   return (
     <>
       <Nav />
