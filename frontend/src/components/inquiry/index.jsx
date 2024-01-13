49bfd61c6109bf103334a7d59d27664c5efbbcbb
Emad Hmady <emadhmady0@gmail.com>
2024-01-13 15:33:20 +0200

    frontend <> created axios request in inquiry controller

--- frontend/src/components/inquiry/index.jsx
+++ frontend/src/components/inquiry/index.jsx
@@ -36,7 +36,7 @@ const Inquiry = () => {
         },
         data: formData,
       });
-      console.log(response.data);
+      console.log(response.data.message);
     } catch (e) {
       console.log(e.response.data);
     }
