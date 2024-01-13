cf112070d3e40f317d68c749740fe47189ddcb07
Emad Hmady <emadhmady0@gmail.com>
2024-01-13 09:21:55 +0200

    frontend <> added isClickable and onClick props to InfoCard component

--- frontend/src/components/infoCard/index.jsx
+++ frontend/src/components/infoCard/index.jsx
@@ -1,9 +1,16 @@
 import "./index.css";
 
 // eslint-disable-next-line react/prop-types
-const InfoCard = ({ icon, title, description }) => {
+const InfoCard = ({ icon, title, description, isClickable, onClick }) => {
   return (
-    <div className="flex column card box-shadow main-color">
+    <div
+      className={
+        isClickable == true
+          ? "flex column card box-shadow main-color"
+          : "flex column card box-shadow main-color clickable"
+      }
+      onClick={isClickable && onClick}
+    >
       <img src={icon} alt="icon" className="card-icon" />
       <p className="card-title">{title}</p>
       <p className="card-description">{description}</p>
