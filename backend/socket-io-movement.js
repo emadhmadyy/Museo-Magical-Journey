1937984d0416443c007c550922a0e6b8d78b6df3
Emad Hmady <emadhmady0@gmail.com>
2024-01-14 09:25:02 +0200

    backend <> created io connection

--- backend/socket-io-movement.js
+++ backend/socket-io-movement.js
@@ -10,6 +10,30 @@ app.get("/", (req, res) => {
 });
 const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173", "https://admin.socket.io"], // front-end URL
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+const cubes = {}; // Store user cubes
+
+io.on("connection", (socket) => {
+  console.log(`A user connected with id: ${socket.id}`);
+
+  // Create a cube for the new user
+  cubes[socket.id] = {
+    x: Math.random() * 10 - 5,
+    z: Math.random() * 10 - 5,
+    rotationX: 0,
+    rotationY: 0,
+    rotationZ: 0,
+  };
+  // Broadcast to all users
+  io.emit("updateState", cubes);
+
 const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
