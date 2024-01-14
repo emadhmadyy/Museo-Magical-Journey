4e2c4300604b8aad74be30296abf7a1a8c2c7049
Emad Hmady <emadhmady0@gmail.com>
2024-01-14 09:38:20 +0200

    backend <> created a new server for realtime messaging

---
+++ backend/socket-io-messaging.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+app.use(cors());
+const { instrument } = require("@socket.io/admin-ui");
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173", "https://admin.socket.io"], // front-end URL
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
