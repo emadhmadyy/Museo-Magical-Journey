f451293180c4c9a5933585da9df30e635f3b985c
Emad Hmady <emadhmady0@gmail.com>
2024-01-14 10:50:51 +0200

    backend <> removed character from create room controller

--- backend/controllers/room.controllers.js
+++ backend/controllers/room.controllers.js
@@ -4,23 +4,9 @@ const Character = require("../models/character.model");
 const { ObjectId } = require("mongoose").Types;
 
 const createRoom = async (req, res) => {
-  const { character_id } = req.body;
-  if (!character_id) {
-    return res.status(400).send({
-      message: "All fields are required",
-    });
-  }
-  if (!ObjectId.isValid(character_id)) {
-    return res.status(400).send({ message: "Character doesn't exist" });
-  }
   try {
-    const char_id = new ObjectId(character_id);
-    const existingCharacter = await Character.findById(char_id);
-    if (!existingCharacter) {
-      return res.status(400).send({ message: "Character doesn't exist" });
-    }
     const room = new Room({
-      users: [{ user: req.user._id, character: character_id, isHost: true }],
+      users: [{ user: req.user._id, isHost: true }],
     });
     await room.save();
     return res
