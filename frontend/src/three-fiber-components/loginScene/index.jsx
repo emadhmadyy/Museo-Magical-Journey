dd505b21a99fd3d197defad831e4c0326f638fba
Emad Hmady <emadhmady0@gmail.com>
2024-01-18 15:15:38 +0200

    frontend <> added canvas to LoginScene Component

--- frontend/src/three-fiber-components/loginScene/index.jsx
+++ frontend/src/three-fiber-components/loginScene/index.jsx
@@ -1,5 +1,14 @@
+import { Canvas } from "@react-three/fiber";
 const LoginScene = () => {
-  return;
+  return (
+    <Canvas
+      style={{
+        height: "60vh",
+        minWidth: "300px",
+        width: "43%",
+      }}
+    ></Canvas>
+  );
 };
 
 export default LoginScene;
