8fc228e499c14d7909c57e059758b0a3e300f554
Emad Hmady <emadhmady0@gmail.com>
2024-01-19 09:15:52 +0200

    frontend <> edited fallback model component color

--- frontend/src/three-fiber-components/modelfallback/index.jsx
+++ frontend/src/three-fiber-components/modelfallback/index.jsx
@@ -5,7 +5,7 @@ const Fallback = (props) => {
     <>
       <mesh {...props}>
         <boxGeometry args={[1, 1, 1, 2, 2, 2]} />
-        <meshBasicMaterial color="red" wireframe />
+        <meshBasicMaterial color="white" wireframe />
       </mesh>
     </>
   );
