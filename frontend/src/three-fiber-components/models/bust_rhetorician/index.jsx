22fb532e6bb4195d905db3d41361cfaaa451ed7e
Emad Hmady <emadhmady0@gmail.com>
2024-01-21 11:32:21 +0200

    frontend <> added preload to bust rhetorician model

--- frontend/src/three-fiber-components/models/bust_rhetorician/index.jsx
+++ frontend/src/three-fiber-components/models/bust_rhetorician/index.jsx
@@ -4,4 +4,5 @@ const BustRhetorician = (props) => {
   const { scene } = useGLTF("./bust_of_a_rhetorician/scene.gltf");
   return <primitive object={scene} {...props} />;
 };
+useGLTF.preload("./bust_of_a_rhetorician/scene.gltf");
 export default BustRhetorician;
