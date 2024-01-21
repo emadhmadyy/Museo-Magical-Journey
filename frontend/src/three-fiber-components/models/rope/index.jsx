848ef1fc113aad679df50e30337349159bd899fe
Emad Hmady <emadhmady0@gmail.com>
2024-01-21 13:44:13 +0200

    frontend <> added preload to Rope model

--- frontend/src/three-fiber-components/models/rope/index.jsx
+++ frontend/src/three-fiber-components/models/rope/index.jsx
@@ -4,5 +4,5 @@ const Rope = (props) => {
   const { scene } = useGLTF("./velvet_rope_low_poly_barrier/scene.gltf");
   return <primitive object={scene} {...props} />;
 };
-
+useGLTF.preload("./velvet_rope_low_poly_barrier/scene.gltf");
 export default Rope;
