c2c1f47a69a9459c8bc0668e3f6bb857703ce970
Emad Hmady <emadhmady0@gmail.com>
2024-01-21 11:04:49 +0200

    frontend <> added preload to bust of zeus

--- frontend/src/three-fiber-components/models/bust_zeus/index.js
+++ frontend/src/three-fiber-components/models/bust_zeus/index.js
@@ -4,4 +4,5 @@ const BustZeus = (props) => {
   const { scene } = useGLTF("./colossal_marble_bust_of_zeus/scene.gltf");
   return <primitive object={scene} {...props} />;
 };
+useGLTF.preload("./colossal_marble_bust_of_zeus/scene.gltf");
 export default BustZeus;
