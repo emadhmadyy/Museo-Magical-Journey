ff25522aca032e2aa93e3e5d83fb4015fb150e48
Emad Hmady <emadhmady0@gmail.com>
2024-01-21 10:01:27 +0200

    frontend <> created nike component

---
+++ frontend/src/three-fiber-components/models/nike/index.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/no-unknown-property */
+import { useGLTF } from "@react-three/drei";
+const Nike = (props) => {
+  const { scene } = useGLTF("./statue_bust_of_nike/scene.gltf");
+  return <primitive object={scene} {...props} />;
+};
+export default Nike;
