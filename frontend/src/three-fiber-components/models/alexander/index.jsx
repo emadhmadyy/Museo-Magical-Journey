342cd7b5244653dc3bd18210b259b20a80e97ed9
Emad Hmady <emadhmady0@gmail.com>
2024-01-21 12:01:24 +0200

    frontend <> created alexander component

---
+++ frontend/src/three-fiber-components/models/alexander/index.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/no-unknown-property */
+import { useGLTF } from "@react-three/drei";
+const Alexander = (props) => {
+  const { scene } = useGLTF(
+    "./alexander_the_great_sets_fire_to_persepolis/scene.gltf"
+  );
+  return <primitive object={scene} {...props} />;
+};
+export default Alexander;
