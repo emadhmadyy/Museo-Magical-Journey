397592ca69fb2e2aeb4ef7a09c0132d26fd907b9
Emad Hmady <emadhmady0@gmail.com>
2024-01-21 11:03:38 +0200

    frontend <> created BustZeus component

---
+++ frontend/src/three-fiber-components/models/bust_zeus/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/no-unknown-property */
+import { useGLTF } from "@react-three/drei";
+const BustZeus = (props) => {
+  const { scene } = useGLTF("./colossal_marble_bust_of_zeus/scene.gltf");
+  return <primitive object={scene} {...props} />;
+};
+export default BustZeus;
