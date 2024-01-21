79e405801163e2e7a23c5a7eaf095abc544b6dc0
Emad Hmady <emadhmady0@gmail.com>
2024-01-21 10:30:49 +0200

    frontend <> added preload to hercules component

--- frontend/src/three-fiber-components/models/hercules_giambologna/index.jsx
+++ frontend/src/three-fiber-components/models/hercules_giambologna/index.jsx
@@ -4,4 +4,5 @@ const HerculesGiambologna = (props) => {
   const { scene } = useGLTF("./flo_hercules_giambologna/scene.gltf");
   return <primitive object={scene} {...props} />;
 };
+useGLTF.preload("./flo_hercules_giambologna/scene.gltf");
 export default HerculesGiambologna;
