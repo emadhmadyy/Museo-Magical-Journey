63b11c5684e919759459f521770e75981fdc54b8
Emad Hmady <emadhmady0@gmail.com>
2024-01-22 11:56:52 +0200

    frontend <> added useEffect to check user authentication in museum component

--- frontend/src/pages/museum/index.jsx
+++ frontend/src/pages/museum/index.jsx
@@ -1,9 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "../../three-fiber-components/experience";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import MessageBox from "../../components/messagebox";
+import { useNavigate } from "react-router-dom";
 
 const Museum = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!localStorage.getItem("token") || !localStorage.getItem("user")) {
+      navigate("/");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <Suspense fallback={<h1>Loading</h1>}>
       <Canvas>
