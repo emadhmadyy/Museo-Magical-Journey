ddf62a80b1aac8109385ae8fc9402ab352029582
Emad Hmady <emadhmady0@gmail.com>
2024-01-22 09:58:41 +0200

    frontend <> iterated over the message object using map()

--- frontend/src/components/messagebox/index.jsx
+++ frontend/src/components/messagebox/index.jsx
@@ -1,9 +1,20 @@
 const MessageBox = () => {
   const messages = [
-    { name: "emad", message: "Hello" },
-    { name: "khaled", message: "Hello everyone" },
+    { name: "emad", message: "Hello", id: 1 },
+    { name: "khaled", message: "Hello everyone", id: 2 },
   ];
-  return <div></div>;
+  return (
+    <div>
+      {messages.map((message) => {
+        return (
+          <div key={message.id}>
+            <p className="sender_name">{message.name}</p>
+            <p className="message">{message.message}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MessageBox;
