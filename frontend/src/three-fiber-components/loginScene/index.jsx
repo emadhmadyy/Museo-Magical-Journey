ccb464b1160d8b37999390e39b4de4d2bb6469e1
Emad Hmady <emadhmady0@gmail.com>
2024-01-24 13:19:47 +0200

    frontend <> removed commented out code from login scene component

--- frontend/src/three-fiber-components/loginScene/index.jsx
+++ frontend/src/three-fiber-components/loginScene/index.jsx
@@ -1,41 +1,24 @@
 /* eslint-disable react/no-unknown-property */
-import { useFrame, useThree } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
 //import { useGLTF } from "@react-three/drei";
 import { MeshReflectorMaterial } from "@react-three/drei";
 import { Suspense, useRef } from "react";
 import Model from "../model";
 import LoadingIndicator from "../loadingIndicator";
-import * as THREE from "three";
-import { AxesHelper } from "three";
 
 const LoginScene = () => {
-  const { camera } = useThree();
-  // camera.position.y = 5;
-  // camera.position.z = 0;
-  // camera.lookAt(new THREE.Vector3(0, 0, 0));
   const login_model_ref = useRef();
   const group = useRef();
-  const offset = [0, -3.2, 0];
   useFrame((state, delta) => {
     if (login_model_ref.current) {
       group.current.rotation.y += delta / 2;
-      //login_model_ref.current.rotation.y += delta / 2;
-      // console.log(login_model_ref.current);
     }
   });
   return (
     <>
       <directionalLight intensity={9} position={[0, 0, 1]} />
       <ambientLight position={[0, 0, 2]} intensity={9} />
-      <Suspense
-        fallback={
-          <LoadingIndicator />
-          //   <mesh position={[0, 0, 1]} scale={[1, 3, 2]}>
-          //     <boxGeometry args={[2, 1, 1, 2, 2, 2]} />
-          //     <meshBasicMaterial color="white" wireframe />
-          //   </mesh>
-        }
-      >
+      <Suspense fallback={<LoadingIndicator />}>
         <group ref={group} scale={2.5}>
           <Model
             url="./bust_of_marcus_aurelius/scene.gltf"
@@ -45,11 +28,6 @@ const LoginScene = () => {
           />
         </group>
       </Suspense>
-      {/* <primitive
-        object={login_model.scene}
-        ref={login_model_ref}
-        scale={0.6}
-      ></primitive> */}
       <mesh scale={[5, 5, 4]} rotation-x={-Math.PI * 0.5} position={[0, -2, 0]}>
         <planeGeometry />
         <MeshReflectorMaterial
