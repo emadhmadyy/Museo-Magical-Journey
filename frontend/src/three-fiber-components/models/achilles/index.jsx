3311d6fc0b19a0f9efc6964911a5b97b4e61238c
Emad Hmady <emadhmady0@gmail.com>
2024-01-25 21:58:09 +0200

    frontend <> added text to achilles statue in the museum

--- frontend/src/three-fiber-components/models/achilles/index.jsx
+++ frontend/src/three-fiber-components/models/achilles/index.jsx
@@ -1,8 +1,28 @@
 /* eslint-disable react/no-unknown-property */
-import { useGLTF } from "@react-three/drei";
+import { Html, useGLTF } from "@react-three/drei";
 const Achilles = (props) => {
   const { scene } = useGLTF("./achilles_heel/scene.gltf");
-  return <primitive object={scene} {...props} />;
+  return (
+    <primitive object={scene} {...props}>
+      <Html
+        position={[0, 0.23, 0]}
+        wrapperClass="museum-label"
+        occlude
+        distanceFactor={15}
+      >
+        <h1>Achilles</h1>
+        <h2>around the 4th century BCE</h2>
+        <p>
+          In Greek mythology, Achilles was the strongest warrior and hero in the
+          Greek army during the Trojan War.
+        </p>
+        <p>
+          He was the son of Peleus, king of the Myrmidons, and Thetis, a sea
+          nymph.
+        </p>
+      </Html>
+    </primitive>
+  );
 };
 useGLTF.preload("./achilles_heel/scene.gltf");
 export default Achilles;
