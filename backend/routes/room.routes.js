3db27e9d3df86c2dfebceafb4a29a615602902a6
Emad Hmady <emadhmady0@gmail.com>
2024-01-27 16:30:55 +0200

    backend <> added /end route to room routes

--- backend/routes/room.routes.js
+++ backend/routes/room.routes.js
@@ -4,9 +4,11 @@ const {
   createRoom,
   joinRoom,
   leaveRoom,
+  endRoom,
 } = require("../controllers/room.controllers");
 router.post("/create", createRoom);
 router.post("/join", joinRoom);
 router.post("/leave", leaveRoom);
+router.post("/end", endRoom);
 
 module.exports = router;
